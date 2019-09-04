31034b382e05b5e21657f38a79dbfda58dad24a6
rhn014 <Roland.Hyden@RegionHalland.se>
2019-09-04 11:12:56 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# region-halland-mce-buttons-with-css
-Skapa formatering-knappar i Wordpress editor inklusive css
+# Skapa formatering-knappar i Wordpress editor inklusive css
+
+## Hur man använder Region Hallands plugin "region-halland-mce-buttons-with-css"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-mce-buttons-with-css".
+
+
+## Användningsområde
+
+Denna plugin skapar formatering-knappar i Wordpress editor + att man kan koppla en css-fil till edotorn för visuell styling
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-mce-buttons-with-css.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-mce-buttons-with-css.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-mce-buttons-with-css": "1.0.0"
+},
+```
+
+
+## Hur du anger namnet på css-filen i .env-filen
+
+OBS! Själva filen ska ligga i foldern "web/app/themes/[THEME-NAME]/resources/"
+
+```sh
+WP_EDITOR_STYLE_CSS=my-editor-style.css
+```
+
+
+### 1.0.0
+- Första version
---
+++ region_halland_mce_buttons_with_css.php
@@ -0,0 +1,90 @@
+<?php
+
+	/**
+	 * @package Region Halland Mce Buttons with Css
+	 */
+	/*
+	Plugin Name: Region Halland Mce Buttons with Css
+	Description: Skapa formatering-knappar i Wordpress editor inklusive css
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: GPL-3.0
+	Text Domain: regionhalland
+	*/
+
+	// Anropa metod för att lägga till js-fil
+	add_action('admin_enqueue_scripts', 'my_enqueue_for_region_halland_mce_buttons_with_css');
+
+	// Metod för att lägga till js-fil
+	function my_enqueue_for_region_halland_mce_buttons_with_css($hook) {
+		
+		// Kontrollera om det finns en angiven css för wordpress editor
+		$myEditorStyle = ENV('WP_EDITOR_STYLE_CSS');
+		$useMyEditorStyle = 0;
+		if (strlen($myEditorStyle) > 0) {
+			$useMyEditorStyle = 1;
+		}
+		
+		// TinyMCE editor styling
+		if ($useMyEditorStyle == 1) {
+			add_editor_style($myEditorStyle);
+		}
+
+	}
+
+	// Callback function to insert 'styleselect' into the $buttons array
+	function my_region_halland_mce_buttons_with_css($buttons) {
+		array_unshift($buttons, 'styleselect');
+		return $buttons;
+	}
+	// Register our callback to the appropriate filter
+	add_filter('mce_buttons_3', 'my_region_halland_mce_buttons_with_css');
+
+	// Callback function to filter the MCE settings
+	function my_region_halland_mce_buttons_with_css_before_init($init_array) {  
+		
+		// Skapa format-knappar
+		$myButtons = array(
+			array(  
+				'title' => 'Blue Text',  
+				'block' => 'span',  
+				'classes' => 'blue-box',
+				'wrapper' => true,
+			),  
+			array(  
+				'title' => 'Red Text',  
+				'block' => 'span',  
+				'classes' => 'red-box',
+				'wrapper' => true,
+			),  
+		);  
+
+		// json-encoda knapparna
+		$init_array['style_formats'] = json_encode($myButtons);  
+		
+		// Returnera knapparna
+		return $init_array;  
+	  
+	} 
+	
+	// Skapa wp-filter "tiny_mce_before_init" 
+	add_filter('tiny_mce_before_init', 'my_region_halland_mce_buttons_with_css_before_init' );
+
+	
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_mce_buttons_with_css_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen avaktiveras
+	function region_halland_mce_buttons_with_css_deactivate() {
+		// Ingenting just nu...
+	}
+	
+	// Vilken metod som ska anropas när pluginen aktiveras
+	register_activation_hook( __FILE__, 'region_halland_mce_buttons_with_css_activate');
+
+	// Vilken metod som ska anropas när pluginen avaktiveras
+	register_deactivation_hook( __FILE__, 'region_halland_mce_buttons_with_css_deactivate');
+
+?>
